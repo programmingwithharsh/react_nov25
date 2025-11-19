f3c955aba11147866163c8a53dc930da574b8786
Harsh Tiwari <harshtiwari343@email.com>
2025-11-19 11:32:43 +0530

    Removed un necesssary files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client'; // Importing es6 module
 
+let message = 'programming with Harsh'
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  message
 )
