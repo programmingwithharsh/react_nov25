25f608d3bf5b32e47ff2e6b9313ea789406b1376
Harsh Tiwari <harshtiwari343@email.com>
2025-11-20 11:46:08 +0530

    Added more styling

--- src/App.jsx
+++ src/App.jsx
@@ -35,9 +35,11 @@ export default class App extends React.Component { // Exporting a component
     render() { // lifecycle
         console.log('Render 2');
         console.log(this.state);
-        let myStyle = { color: 'white', backgroundColor: 'green' };
+        let myStyle = { color: 'white', backgroundColor: 'green', border: "2px solid red", margin: "10px" };
         return <div>
             <div style={myStyle}>This is App class Component</div>
+            <div style={myStyle}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit eaque minus reprehenderit explicabo laudantium ducimus iste perferendis nihil eos nulla, vitae nesciunt ullam magnam esse. Molestias quisquam minus amet numquam!</div>
+
             <div style={{ color: 'white', backgroundColor: 'blue' }}><b>State username is {this.state.username}</b></div>
             <button onClick={this.updateUsername}>Update username state</button>
             <AddProduct />
