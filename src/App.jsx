ed565d81bfe3429ab898843d3d3242fc945a4b03
Harsh Tiwari <harshtiwari343@email.com>
2025-11-20 10:23:17 +0530

    Added class component lifecycle

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,22 @@ import Title from "./Title";
 import Star from "./Star";
 
 export default class App extends React.Component { // Exporting a component
+    constructor() { // component lifecycle
+        super();
+        console.log('Constructor 1');
+    }
+    componentDidMount() {
+        console.log('ComponentDidMount 3');
+        /*
+            This is the best place for calling the APIs, it calls after render
+
+            APIs calls in React 
+                Javascript fetch method
+                Axios module 
+        */
+    }
     render() { // lifecycle
+        console.log('Render 2');
         return <div>
             <div>This is App class Component</div>
             <AddProduct />
